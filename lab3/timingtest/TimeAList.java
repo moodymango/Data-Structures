ec7690d2cbb95acf98bda79133a8612d3bb3fc7d
imz <duv.imma@gmail.com>
2024-02-13 17:17:33 -0500

    add timeAListConstruction method to TimeList java file

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,6 +1,8 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
 
+import java.util.ArrayList;
+
 /**
  * Created by hug.
  */
@@ -22,6 +24,35 @@ public class TimeAList {
     }
 
     public static void timeAListConstruction() {
-        // TODO: YOUR CODE HERE
+       //instantiate an array of size according to the table
+        int [] sizes = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 120000};
+        //instantiate 3 Array lists to pass into print timing table function
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        //iterate through the sizes array with for in loop
+         for(Integer size : sizes) {
+             //add the current size to the ns and the opCounts list
+             Ns.addLast(size);
+             opCounts.addLast(size);
+             //need to calculate the time it takes for the AList functionality to work
+             //instanstiate a dummy AList for the sole purpose of calculating how long it takes to complete add last function
+             AList<Integer> dummyList = new AList<>();
+             //create a loop to iterate from 1 to current size to add
+             //initialize counter variable
+             int counter = 1;
+             //declare stopwatch variable and assign it the location of a stopwatch instance prior to the start of the loop
+             Stopwatch sw = new Stopwatch();
+             while (counter <= size) {
+                 dummyList.addLast(counter);
+                 counter++;
+             }
+             //declare double variable time in seconds to capture how much time has passed after calling the function
+             double timeInSeconds = sw.elapsedTime();
+             //add the timeInSeconds variable to the times AList
+             times.addLast(timeInSeconds);
+         }
+            // call the print function passing in the three ALists
+        printTimingTable(Ns, times, opCounts);
     }
 }
