f8338937f30b710dcd1ca9f6ef3be2a67c1af4e7
imz <duv.imma@gmail.com>
2024-02-13 23:25:43 -0500

    add code in BuggyAList to fix bug in buggyAList

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -29,19 +29,29 @@ public class TestBuggyAList {
   @Test
   public void randomizedTest() {
       AListNoResizing<Integer> L = new AListNoResizing<>();
+      BuggyAList<Integer> buggyL = new BuggyAList<>();
 
-      int N = 500;
+      int N = 5000;
       for (int i = 0; i < N; i += 1) {
-          int operationNumber = StdRandom.uniform(0, 2);
+          int operationNumber = StdRandom.uniform(0, 4);
           if (operationNumber == 0) {
               // addLast
               int randVal = StdRandom.uniform(0, 100);
               L.addLast(randVal);
-              System.out.println("addLast(" + randVal + ")");
+              buggyL.addLast(randVal);
           } else if (operationNumber == 1) {
               // size
               int size = L.size();
-              System.out.println("size: " + size);
+              int buggySize = buggyL.size();
+              assertEquals(size, buggySize);
+          } else if (operationNumber ==  2) {
+              if (L.size() > 0) {
+                  assertEquals(L.getLast(), buggyL.getLast());
+              }
+          } else {
+              if (L.size() > 0) {
+                  assertEquals(L.removeLast(), buggyL.removeLast());
+              }
           }
       }
   }
