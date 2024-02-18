7b45b1951457c8df54f39cfa9383d585e25a79d7
imz <duv.imma@gmail.com>
2024-02-17 19:26:54 -0500

    add skeleton code for MaxArrayDeque

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,19 @@
+package deque;
+
+import java.util.Comparator;
+
+/*maxArrayDeque serves as a hyponym of arrayDeque*/
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+   /*creates a maxArrayDeque with the given comparator*/
+    public MaxArrayDeque(Comparator<T> c) {
+
+    }
+   /*returns the max el in the deque as governed by the previously given comparator*/
+    public T max() {
+        return null;
+    }
+  /* returns the max el in the deque as governed by the param c*/
+    public T max(Comparator<T> c ) {
+        return null;
+    }
+}
